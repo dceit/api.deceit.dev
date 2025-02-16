c5cd83dfb30a41b8b9c9c2543e01595ce4d0c37b
dceit <72780993+dceit@users.noreply.github.com>
2025-02-16 15:25:35 -0500

    Implement CORS for external API calls

--- index.js
+++ index.js
@@ -55,6 +55,11 @@ async function osuGetChatEntries(req, res) {
 const server = restify.createServer();
 
 server.use(restify.plugins.queryParser());
+server.use(function crossOrigin(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  return next();
+});
 
 server.get("/ping", respond);
 server.get("/api/osu/chat", osuGetChatEntries);
